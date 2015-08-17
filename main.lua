dc38abcaa042a235d963b941873962921489fd92
GeoDashup <juanismael06@gmail.com>
2015-08-17 17:46:24 +0200

    Añadiendo imágenes
    Añadiendo imágenes

--- main.lua
+++ main.lua
@@ -23,7 +23,7 @@ local pelota = display.newCircle( 65, 170, 15 )
 physics.addBody( pelota, "dynamic", {density = 0.9, friction=0.2 ,bounce=0.7 } )
 
 
-local suelo = display.newRect( centerX, alto, ancho, 10 )
+local suelo = display.newRect( -5, alto, ancho, 10 )
 
 physics.addBody(suelo, "static", {density = 0.9, friction=0.1 ,bounce=0.8 } )
 
@@ -31,7 +31,7 @@ for i = 1, 4 do
 
 local bloque = display.newRect(ancho/4 + 50, centerY - 150 + 30 * i , 30, 30)
 
-physics.addBody( bloque, "dynamic", {density = 0.5, friction=0.2 ,bounce=0.1 } )
+physics.addBody( bloque, "dynamic", {density = 0.5, friction=0.0 ,bounce=0.1 } )
 
 end
 
@@ -40,7 +40,27 @@ local plataforma = display.newRect( ancho/4, centerY, ancho/2, 10)
 
 physics.addBody (plataforma, "static", {density = 0.9, friction=0.1 ,bounce=0.8 })
 
+local plataforma = display.newRect( ancho/4, 450, ancho/2, 10)
 
+physics.addBody (plataforma, "static", {density = 0.9, friction=0.1 ,bounce=0.8 })
+
+for i = 0, 2 do
+
+local rect = display.newRect(150, 370 + i * 20, 20, 20)
+physics.addBody( rect, "dynamic", {density = 1.0, friction=0.3 ,bounce=0.2 } )
+
+end
+
+
+--Colocando paredes
+local pared = display.newRect( 345, 345, 4, 800 )
+physics.addBody( pared, "static",  {density = 0.9, friction=0.1 ,bounce=0.8 } )
+
+local pared = display.newRect( -25, -25, 4, 1000 )
+physics.addBody( pared, "static",  {density = 0.9, friction=0.1 ,bounce=0.8 } )
+
+
+--Movimientos pelota
 function circleTouched(event)
 if event.phase == "began" then
 	display.getCurrentStage( ):setFocus(pelota)
