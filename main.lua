ac72d8616b0aefb256792c69a66cd158c63c8dd4
GeoDashup <juanismael06@gmail.com>
2015-08-18 17:37:31 +0200

    Poniendo imágenes
    Poniendo imágenes

--- main.lua
+++ main.lua
@@ -18,38 +18,30 @@ physics.start( )
 
 
 
-local pelota = display.newCircle( 65, 170, 15 )
+local pelota = display.newImage( "Futbol.png", 65, 15 )
 
 physics.addBody( pelota, "dynamic", {density = 0.9, friction=0.2 ,bounce=0.7 } )
 
 
-local suelo = display.newRect( -5, alto, ancho, 10 )
+local suelo = display.newRect( 150, alto, ancho, 10 )
 
 physics.addBody(suelo, "static", {density = 0.9, friction=0.1 ,bounce=0.8 } )
 
 for i = 1, 4 do
 
-local bloque = display.newRect(ancho/4 + 50, centerY - 150 + 30 * i , 30, 30)
+local bloque = display.newImage( "cloud.png", ancho/4 + 50, centerX - 150 + 30 * i , 20, 30)
 
-physics.addBody( bloque, "dynamic", {density = 0.5, friction=0.0 ,bounce=0.1 } )
+physics.addBody( bloque, "dynamic", {density = 0.5, friction=0.20 ,bounce=0.5} )
 
 end
 
 --plataforma1
-local plataforma = display.newRect( ancho/4, centerY, ancho/2, 10)
+local plataforma = display.newRect( ancho/4, centerX, ancho/2, 10)
 
 physics.addBody (plataforma, "static", {density = 0.9, friction=0.1 ,bounce=0.8 })
 
-local plataforma = display.newRect( ancho/4, 450, ancho/2, 10)
 
-physics.addBody (plataforma, "static", {density = 0.9, friction=0.1 ,bounce=0.8 })
 
-for i = 0, 2 do
-
-local rect = display.newRect(150, 370 + i * 20, 20, 20)
-physics.addBody( rect, "dynamic", {density = 1.0, friction=0.3 ,bounce=0.2 } )
-
-end
 
 
 --Colocando paredes
@@ -60,8 +52,22 @@ local pared = display.newRect( -25, -25, 4, 1000 )
 physics.addBody( pared, "static",  {density = 0.9, friction=0.1 ,bounce=0.8 } )
 
 
+for i = 0, 2 do
+
+local rect = display.newRect( 150, 370 + i * 20, 20, 20 )
+physics.addBody( rect, "dynamic", {density = 0.9, friction=0.1 ,bounce=0.8 } )
+
+
+end
+
+
+
+
+
+
 --Movimientos pelota
 function circleTouched(event)
+
 if event.phase == "began" then
 	display.getCurrentStage( ):setFocus(pelota)
 elseif event.phase == "ended" then
